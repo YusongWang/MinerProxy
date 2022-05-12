6a33820b53e324f7eb711b698897a7e09b3ee352
Wangyusong <admin@wangyusong.com>
2022-05-12 16:28:45 +0800

    fix

---
+++ global/fee.go
@@ -0,0 +1,19 @@
+package global
+
+import (
+	"io"
+	"sync"
+)
+
+type FeeConn struct {
+	DevConn *io.ReadWriteCloser
+	FeeConn *io.ReadWriteCloser
+}
+
+type FeeResult struct{}
+
+// 保存临时份额 查找时间O(1)
+type Fee struct {
+	Dev sync.Map
+	Fee sync.Map
+}
